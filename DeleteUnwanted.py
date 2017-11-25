617c350b7b8fa2119e9bf1267b6ac3c931ea071b
aa1000 <ahmedfayez37@gmail.com>
2017-11-25 09:59:27 +0200

    Add files via upload

---
+++ DeleteUnwanted.py
@@ -0,0 +1,18 @@
+import os, os.path
+from glob import glob
+
+file_path = input('enter text file path: ')
+
+with open(file_path) as f:
+	for line in f:
+		print('line : ' + line)
+		#line = f.readline()
+		if os.path.isfile(line):
+			os.remove(line)
+			print('Deleting ' + line)
+			text_name = line.split('.')[0] + '.txt'
+			if os.path.isfile(text_name):
+				os.remove(text_name)
+				print('Deleting ' + text_name)
+		else:
+			print('Found Nothing')
